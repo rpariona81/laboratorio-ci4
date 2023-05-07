055fb95f95f38413565af6be0b685f0d0fe1a3d8
rpariona81 <ronald.pariona.isidro@gmail.com>
2023-05-07 11:51:51 -0500

    arreglo de navbar

--- app/Views/admin/layout/main.php
+++ app/Views/admin/layout/main.php
@@ -25,21 +25,19 @@
     <!--<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">-->
     <nav class="sb-topnav navbar navbar-expand navbar-default bg-warning">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3 text-black" href="index.html">
+        <a class="navbar-brand ps-3 text-black" href="#">
             <strong>Bolsa Laboral RRP</strong></a>
 
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
 
         <!-- Navbar-->
-        <ul class="navbar-nav d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+        <ul class="navbar-nav ms-auto me-0 me-md-3 my-2 my-md-0">
             <li class="nav-item dropdown">
-
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <?= $this->renderSection('userlogged') ?>
-                    <i class="fas fa-user fa-fw"></i>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <a class="nav-link dropdown-toggle text-black" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <strong><?= $this->renderSection('userlogged') ? : 'Usuario - Rol' ?>&nbsp;&nbsp;</strong><i class="fas fa-user fa-fw"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                     <li><a class="dropdown-item" href="#!">Mi perfil</a></li>
                     <li><a class="dropdown-item" href="#!">Cambiar clave</a></li>
                     <li>
@@ -77,7 +75,7 @@
                             Estudiantes/Egresados
                         </a>
                         <a class="nav-link collapsed" href="/admin/teachers/list">
-                            <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                            <div class="sb-nav-link-icon"><i class="fas fa-book-reader"></i></div>
                             Docentes
                         </a>
                         <a class="nav-link" href="/admin/vacantes/list">
@@ -85,8 +83,8 @@
                             Convocatorias
                         </a>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                            Reportes
+                            <div class="sb-nav-link-icon"><i class="fas fa-clipboard-list"></i></div>
+                            Postulaciones
                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                         </a>
                         <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
@@ -95,7 +93,14 @@
                                 <a class="nav-link" href="/admin/reportes/fechas">Por rango de fechas</a>
                             </nav>
                         </div>
-                        
+                        <a class="nav-link" href="#">
+                            <div class="sb-nav-link-icon"><i class="fas fa-address-card"></i></div>
+                            Mi perfil
+                        </a>
+                        <form class="ml-4">
+                            &nbsp;&nbsp;&nbsp;<input class="btn btn-primary" id="btnLogout" type="submit" value="Cerrar sesión"></input>
+                        </form>
+
                     </div>
                 </div>
 
